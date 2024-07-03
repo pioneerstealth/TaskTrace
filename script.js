1022106a77e0eb426cb97edaacbfa4e2a53a0dab
RiyaJose1207 <riyamaryjose12@gmail.com>
2024-07-03 14:16:45 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const createTaskButton = document.querySelector('.create-task-button');
+    createTaskButton.addEventListener('click', () => {
+        alert('Task creation logic goes here.');
+    });
+});
